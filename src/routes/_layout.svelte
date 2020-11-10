75deeacbae4ffe4a168c8a88efdbd9856d1123a0
Isaac Morrier <isaacmorrier@gmail.com>
2020-11-09 21:26:19 -0500

    refactor reactive variables and add ellipse

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -6,6 +6,4 @@
 
 </style>
 
-<main>
-	<slot></slot>
-</main>
+<slot></slot>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,76 +2,108 @@
 	import * as THREE from 'three';
 	import { onMount } from 'svelte';
 
-	let w = {w: 100, h: 100} // window dimensions
-	$: c = {x: w.w/2, y: w.h / 2} // window center
-	let m = {x: 0, y: 0} // mouse position
-	let f = {x: 0, y: 0} // face angle
+	let w;
+	let h;
+	let y; // body dimensions and scroll
+	let canvasTarget = "container";
+	let a = {w: 100, h: 100}; // art dimensions
+	$: 	c = {x: a.w / 2, y: a.h / 2}; // center, updates when art width or height changes
+	let m = {x: 0, y: 0}; // mouse position
+	let f = {x: 0, y: 0}; // face angle
+	$:	{
+		f.x = (m.x - c.x) / c.x;
+		f.y = (m.y + y - c.y) / c.y;
+	}
 
 	function handleMousemove(e) {
 		m.x = e.clientX;
 		m.y = e.clientY;
-
-		f.x = (m.x - c.x) / c.x;
-		f.y = (m.y - c.y) / c.y;
-	}
-	function handleResize() {
-		w.w = window.innerWidth;
-		w.h = window.innerHeight;
 	}
 
-	onMount(() => {
-		w.w = window.innerWidth;
-		w.h = window.innerHeight;
-		m.x = window.innerWidth / 2;
-		m.y = window.innerheight / 2;
+	function draw() {
+		a.w = w;
+		a.h = h;
+		m.x = a.w / 2;
+		m.y = a.h / 2;
 		const scene = new THREE.Scene();
-		var camera = new THREE.PerspectiveCamera( 75, w.w / w.h, 0.1, 1000 );
+		var camera = new THREE.PerspectiveCamera( 75, a.w / a.h, 0.1, 1000 );
 		
 		var renderer = new THREE.WebGLRenderer();
-		renderer.setSize( w.w, w.h );
-		document.body.appendChild( renderer.domElement );
+		renderer.setSize( a.w, a.h );
+		document.getElementById(canvasTarget).appendChild( renderer.domElement );
 
-		var geometry = new THREE.BufferGeometry();
-		// create a simple square shape. We duplicate the top left and bottom right
-		// vertices because each vertex needs to appear once per triangle.
-		var vertices = new Float32Array( [
-			-1.0, -1.0,  1.0,
-			1.0, -1.0,  1.0,
-			1.0,  1.0,  1.0,
+		// var geometry = new THREE.BufferGeometry();
+		// // create a simple square shape. We duplicate the top left and bottom right
+		// // vertices because each vertex needs to appear once per triangle.
+		// var vertices = new Float32Array( [
+		// 	-1.0, -1.0,  1.0,
+		// 	1.0, -1.0,  1.0,
+		// 	1.0,  1.0,  1.0,
 
-			1.0,  1.0,  1.0,
-			-1.0,  1.0,  1.0,
-			-1.0, -1.0,  1.0
-		] );
+		// 	1.0,  1.0,  1.0,
+		// 	-1.0,  1.0,  1.0,
+		// 	-1.0, -1.0,  1.0
+		// ] );
 
-		// itemSize = 3 because there are 3 values (components) per vertex
-		geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
-		var material = new THREE.MeshBasicMaterial( { color: 0xffdd99 } );
-		var cube = new THREE.Mesh( geometry, material );
-		scene.add( cube );
+		// // itemSize = 3 because there are 3 values (components) per vertex
+		// geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+		// var material = new THREE.MeshBasicMaterial( { color: 0xffdd99 } );
+		// var cube = new THREE.Mesh( geometry, material );
+		// scene.add( cube );
+
+		const geometry = new THREE.CircleBufferGeometry( 1, 32 );
+		// const p = new THREE.Vector3(1, 0, 0);
+		// geometry.position(p);
+		const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+		const circle = new THREE.Mesh( geometry, material );
+		scene.add( circle );
 
 		camera.position.z = 5;
 
+		function resize() {
+		if (a.w != w || a.h != h) {
+			a.w = w;
+			a.h = h;
+			renderer.setSize( a.w, a.h );
+			camera.aspect =  a.w / a.h;
+			camera.updateProjectionMatrix();
+		}
+	}
+
 		function animate() {
+			resize();
 			requestAnimationFrame( animate );
-			cube.rotation.x = f.y;
-			cube.rotation.y = f.x;
+			circle.rotation.x = f.y;
+			circle.rotation.y = f.x;
 			renderer.render( scene, camera );
 		}
 		animate();
+	}
+
+	onMount(() => {
+		draw();
 	});
+
 </script>
 
-<style lang="scss">
-	:global(body) {
+<style lang="scss" global>
+	body {
 		margin: 0;
+		width: 100%;
+		height: 100%;
+		background: green;
+	}
+	#container {
+		width: 100%;
+		height: 200vh;
+		background: blue;
 	}
 </style>
-
-<svelte:window on:resize={handleResize} />
 
 <svelte:head>
 	<title>Portals</title>
 </svelte:head>
 
-<svelte:body on:mousemove={handleMousemove} />
+<svelte:window bind:scrollY={y}/>
+
+<div id="container" bind:clientWidth={w} bind:clientHeight={h} on:mousemove={handleMousemove}></div>
